d51ef83a436e39b2b5c836d5044ef964965f66ba
Winkermann <willmeschke@gmail.com>
2023-11-26 18:54:59 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <h1>Bookkeeper!</h1>;
+  return (
+    <>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </>
+  );
 }
 
 export default App;
