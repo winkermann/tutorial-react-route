bbe7cacd72f4103986f332a5f47570913782434c
Winkermann <willmeschke@gmail.com>
2023-11-26 19:36:34 -0300

    no match route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoces/index.tsx";
+import NotFound from "./routes/NotFound/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   </BrowserRouter>
---
+++ src/routes/NotFound/index.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <p>There's nothing here! 😵</p>
+    </main>
+  );
+}
