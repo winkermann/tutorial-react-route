11c82049ca674ff3034ebb476ff97580468e5e1b
Winkermann <willmeschke@gmail.com>
2023-11-27 12:28:48 -0300

    index routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,17 @@ import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoces/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoces/Invoice/index.tsx";
+import InvoicesIndex from "./routes/Invoces/InvoicesIndex/index.tsx";
+import Welcome from "./routes/Welcome/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
+        <Route index element={<Welcome />} />
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
         <Route path="*" element={<NotFound />} />
---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h2>Welcome</h2>
+    </main>
+  );
+}
